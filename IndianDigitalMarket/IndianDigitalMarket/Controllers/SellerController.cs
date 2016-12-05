ad035e3b8343ad34b0656e06daead2f20d758cec
Akash Wadhankar <wadhankar.akash@gmail.com>
2016-12-05 06:49:53 -0500

    Added API to BuyerController and added some class libraries for changing structure of project.

--- IndianDigitalMarket/IndianDigitalMarket/Controllers/SellerController.cs
+++ IndianDigitalMarket/IndianDigitalMarket/Controllers/SellerController.cs
@@ -17,31 +17,63 @@ namespace IndianDigitalMarket.Controllers
             return new string[] { "value1", "value2" };
         }
 
+        //Creates shop for seller
         public void RegisterShop()
         { }
 
-        public void EditShop()
+        //Modify shop for seller
+        public void ModifyShop()
         { }
 
+        //View shops owned by me
+        public void ViewMyShop()
+        { }
 
+        //View Products in My Shop
+        public void ViewMyProducts()
+        { }
+
+        //Add the product you want to sell
         public void AddproductToShop()
         { }
 
-        public void AddServicePincodes()
+        //Add the service pincodes to products
+        public void AddServicePincodesToProduct()
         { }
 
-
+        //View Orders posted by customers
         public void ViewOrder()
         { }
 
+        //Verify order from customer  
+        public void VerifyOrderDetails()
+        { }
+
+        //Accept orders - (Orders Staging to Order Master)
         public void AcceptOrder()
         { }
 
+        //Assign order to Shop Employees to full fill
         public void AssignOrder()
         { }
 
-        public void ViewWallet()
+        //CompleteOrder - debit order amount from buyer wallet and add to seller wallet
+        public void CompleteOrder()
         { }
+
+        //Send Bill Monthly
+        public void SendMonthlyBill()
+        { }
+
+        //Record Cash Payment - Make the recorder debit and credit entry to wallet null and void
+        public void RecordCashPayment()
+        { }        
+
+        //View all the Buys and Sells - Earning - Number of Cash Payments/Pending Payments/Full filled payments
+        public void ViewMonthlyReport()
+        { }
+
+
 
     }
 }
