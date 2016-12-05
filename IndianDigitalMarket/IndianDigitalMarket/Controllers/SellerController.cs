84bb00ae9ed13f88e34bbc99034cfd1c0cde29bd
Akash Wadhankar <wadhankar.akash@gmail.com>
2016-12-04 21:44:16 -0500

    adding Seller API class

---
+++ IndianDigitalMarket/IndianDigitalMarket/Controllers/SellerController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+using System.Web.Http;
+
+
+namespace IndianDigitalMarket.Controllers
+{
+    public class SellerController : ApiController
+    {
+        [System.Web.Http.AcceptVerbs("GET")]
+        public string[] RegisterSeller()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        public void RegisterShop()
+        { }
+
+        public void EditShop()
+        { }
+
+
+        public void AddproductToShop()
+        { }
+
+        public void AddServicePincodes()
+        { }
+
+
+        public void ViewOrder()
+        { }
+
+        public void AcceptOrder()
+        { }
+
+        public void AssignOrder()
+        { }
+
+        public void ViewWallet()
+        { }
+
+    }
+}
