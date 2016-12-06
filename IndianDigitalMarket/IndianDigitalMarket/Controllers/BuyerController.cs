c018f1973a2b3e17c6c28b853164682838155417
Akash Wadhankar <wadhankar.akash@gmail.com>
2016-12-05 21:10:12 -0500

    commit and push

--- IndianDigitalMarket/IndianDigitalMarket/Controllers/BuyerController.cs
+++ IndianDigitalMarket/IndianDigitalMarket/Controllers/BuyerController.cs
@@ -9,5 +9,33 @@ namespace IndianDigitalMarket.Controllers
 {
     public class BuyerController : ApiController
     {
+        public void RegisterBuyer()
+        { }
+
+        public void SaveMyPincodes()
+        { }
+        
+        public void GetMyPincodes()
+        { }
+
+        //View All Shops that serve me by pincode- Back service for Search screen
+        //View All Shops that serve me and my product
+        public void ViewAllShopsAndProducts()
+        { }
+
+        //View All products in shop
+        public void ViewAllProductsInShop() { }
+
+        //View Product Details
+        public void ViewProductDetails() { }
+
+        //OrderProduct
+        //ViewAllMyOrdersWithStatus
+        //ViewMyWalletHistory
+        //ViewMyBalance
+        //RecordCashPayment
+        //MakePayment - Kake payment with Paypal or mastercard
+        //ConfirmOrderCompletion
+        //ViewMyDues
     }
 }
